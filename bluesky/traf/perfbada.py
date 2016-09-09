11c68f23e0739bdcfd709bb2a127764aae7f5057
eman89 <emmanuelsunil89@gmail.com>
2016-09-09 18:44:28 +0200

    Revert "Merge remote-tracking branch 'refs/remotes/ProfHoekstra/master'"
    This reverts commit 60ced7224d46889ac076abf96c1f2169bdca9f41, reversing
    changes made to 15abe90d10e8bef55368c8dcd73eeb1adc2a37d1.

--- bluesky/traf/perfbada.py
+++ bluesky/traf/perfbada.py
@@ -402,11 +402,6 @@ class PerfBADA():
         # prepare for coefficient readin
         coeff.coeff()
 
-        # Flight performance scheduling
-        self.dt  = 0.1           # [s] update interval of performance limits
-        self.t0  = -self.dt  # [s] last time checked (in terms of simt)
-        self.warned2 = False        # Flag: Did we warn for default engine parameters yet?
-
         return
 
     def engchange(self, acid, engid=None):
@@ -531,8 +526,7 @@ class PerfBADA():
        
 
 
-    def create(self):
-        actype = self.traf.type[-1]
+    def create(self, actype):
         """CREATE NEW AIRCRAFT"""
         # note: coefficients are initialized in SI units
 
@@ -836,11 +830,7 @@ class PerfBADA():
         return
 
 
-    def perf(self,simt):
-        if abs(simt - self.t0) >= self.dt:
-            self.t0 = simt
-        else:
-            return
+    def perf(self):
         """AIRCRAFT PERFORMANCE"""
         # BADA version
         swbada = True
@@ -977,7 +967,7 @@ class PerfBADA():
         # switch for given vertical speed avs
         if (self.traf.avs.any()>0) or (self.traf.avs.any()<0):
             # thrust = f(avs)
-            T = ((self.traf.avs!=0)*(((self.traf.pilot.vs*self.mass*g0)/     \
+            T = ((self.traf.avs!=0)*(((self.traf.desvs*self.mass*g0)/     \
                       (self.ESF*np.maximum(self.traf.eps,self.traf.tas)*cpred)) \
                       + self.D)) + ((self.traf.avs==0)*T)
                       
@@ -1061,7 +1051,7 @@ class PerfBADA():
         self.ff = np.maximum.reduce([ffto, ffic, ffcc, ffcrl, ffcd, ffap, ffld, ffgd])/60. # convert from kg/min to kg/sec
 
         # update mass
-        self.mass = self.mass - self.ff*self.dt # Use fuelflow in kg/min
+        self.mass = self.mass - self.ff*self.traf.perfdt # Use fuelflow in kg/min
         
         
         
@@ -1098,14 +1088,14 @@ class PerfBADA():
 
         self.hact = self.hmax+self.gt*c1def+self.gw*(self.mmax-self.mass)
         # if hmax in OPF File ==0: hmaxact = hmo, else minimum(hmo, hmact)       
-        self.hmaxact = (self.hmax==0)*self.hmo +(self.hmax !=0)*np.minimum(self.hmo, self.hact)
-
+        self.hmaxact = (self.hmax==0)*self.hmo +(self.hmax !=0)*np.minimum(self.hmo, self.hact)  
+        
         # forwarding to tools
         self.traf.limspd, self.traf.limspd_flag, self.traf.limalt, self.traf.limvs, self.traf.limvs_flag = \
-        limits(self.traf.pilot.spd, self.traf.limspd, self.traf.gs,self.vmto, self.vmin, \
+        limits(self.traf.desspd, self.traf.limspd, self.traf.gs,self.vmto, self.vmin, \
         self.vmo, self.mmo, self.traf.M, self.traf.alt, self.hmaxact, \
-        self.traf.pilot.alt, self.traf.limalt, self.maxthr, self.Thr,self.traf.limvs, \
-        self.D, self.traf.tas, self.mass, self.ESF)        
+        self.traf.desalt, self.traf.limalt, self.maxthr, self.Thr,self.traf.limvs, \
+        self.D, self.traf.tas, self.mass, self.ESF)   
         
         return
 
@@ -1122,7 +1112,7 @@ class PerfBADA():
         #DEBUGGING
 
         #record data 
-        # self.log.write(self.dt, str(self.traf.alt[0]), str(self.traf.tas[0]), str(self.D[0]), str(self.T[0]), str(self.ff[0]),  str(self.traf.vs[0]), str(cd[0]))
+        # self.log.write(self.traf.perfdt, str(self.traf.alt[0]), str(self.traf.tas[0]), str(self.D[0]), str(self.T[0]), str(self.ff[0]),  str(self.traf.vs[0]), str(cd[0]))
         # self.log.save()
 
         # print self.id, self.phase, self.alt/ft, self.tas/kts, self.cas/kts, self.M,  \
