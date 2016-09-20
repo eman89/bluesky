fd4736a2adf8de1ef8c450a35119df2f391b964f
eman89 <emmanuelsunil89@gmail.com>
2016-09-20 22:45:36 +0200

    SNAPLOG implementation complete

--- bluesky/tools/logHeader.py
+++ bluesky/tools/logHeader.py
@@ -7,31 +7,31 @@ Contains functions to write the header for the different log files
 
 def skyHeader():
     
-    parameters = " nTraf [-], Number of current Conflicts [-], Number of Current Intrusions [-]\n"
+    parameters = "nTraf [-], Number of current Conflicts [-], Number of Current Intrusions [-]"
     
-    lines = " ################################################### #\n"+ \
-            " SKY LOG\n" + \
-            " ################################################### #\n\n" + \
-            " Units for parameters:\n" + \
-            parameters
+    lines = "##################### #\n"+ \
+            "SKY LOG\n" + \
+            "##################### #\n\n" + \
+            "Units for parameters:\n" + \
+            parameters + "\n"
     
     return lines
 
 
 def snapHeader():
 
-    parameters = " Call Sign [-], Spawn Time [s], AC Type [-], Latitude [deg],"   + \
-                 " Longitude [deg], Altitude [m], TAS [m/s], GS [m/s], VS [m/s]," + \
-                 " HDG [deg], TRK [deg], CMDALT [m], CMDTAS [m/s], CMDTRK [deg]," + \
-                 " CMDVS [m/s], ASAS Active [-], ASASALT [m], ASASTAS [m/s],"     + \
-                 " ASASTRK [deg], ASASVS [m/s], LNAV Active [-], VNAV Active [-],"+ \
-                 " Origin [-], Destination [-]"
+    parameters = "Call Sign [-], Spawn Time [s], AC Type [-], Latitude [deg], "   + \
+                 "Longitude [deg], Altitude [m], TAS [m/s], GS [m/s], VS [m/s], " + \
+                 "HDG [deg], TRK [deg], CMDALT [m], CMDTAS [m/s], CMDTRK [deg], " + \
+                 "CMDVS [m/s], ASAS Active [-], ASASALT [m], ASASTAS [m/s], "     + \
+                 "ASASTRK [deg], ASASVS [m/s], LNAV Active [-], VNAV Active [-], "+ \
+                 "Origin [-], Destination [-]"
     
-    lines = " ################################################### #\n"+ \
-            " SNAP LOG\n" + \
-            " ################################################### #\n\n" + \
-            " Units for parameters:\n" + \
-            parameters
+    lines = "##################### #\n"+ \
+            "SNAP LOG\n" + \
+            "##################### #\n\n" + \
+            "Units for parameters:\n" + \
+            parameters + "\n"
     
     return lines
 
