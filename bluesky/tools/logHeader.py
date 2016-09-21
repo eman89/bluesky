5cdf81d6e542f78531da498590f3dcf8d0861206
eman89 <emmanuelsunil89@gmail.com>
2016-09-21 14:49:17 +0200

    CFL log header function

--- bluesky/tools/logHeader.py
+++ bluesky/tools/logHeader.py
@@ -40,7 +40,31 @@ def snapHeader():
 
 def cflHeader():
     
-    parameters = "Call Sign id1 [-],"
+    parameters =  "Call Sign id1 [-], Latitude id1 [deg], Longitude id1 [deg], "   + \
+                  "Altitude id1 [m], TAS id1 [m/s], VS id1 [m/s], HDG id1 [deg], " + \
+                  "ASAS Active id1 [-], ASASTAS id1 [m/s], ASASTRK id1 [deg], "    + \
+                  "CPA LAT id1 [deg], CPA LON id1 [deg], CPA ALT id1 [m], "        + \
+                  "Tinconf id1 [s], Toutconf id1 [s], Tcpa id1[s],"                + \
+                  "n active Conflicts with id1, "                                  + \
+                  "Call Sign id2 [-], Latitude id2 [deg], Longitude id2 [deg], "   + \
+                  "Altitude id2 [m], TAS id2 [m/s], VS id2 [m/s], HDG id2 [deg], " + \
+                  "ASAS Active id2 [-], ASASTAS id2 [m/s], ASASTRK id1 [deg], "    + \
+                  "CPA LAT id2 [deg], CPA LON id2 [deg], CPA ALT id2 [m], "        + \
+                  "Tinconf id2 [s], Toutconf id2 [s], Tcpa id2[s], "               + \
+                  "n active Conflicts with id1"
+    
+    lines      = "##################### #\n"    + \
+                 "CFL LOG\n"                   + \
+                 "##################### #\n\n"  + \
+                 "Units for parameters:\n"      + \
+                 parameters + "\n"              + \
+                 "NOTE: ORDER OF PARAMETERS VARIES FROM ABOVE LIST!\n" + \
+                 "      See below for actual order\n"
+    
+    return lines
+                  
+
+
 
     
     
