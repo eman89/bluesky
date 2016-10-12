ce75de4e7a9d6fb0e09217e047ce98d3d0f01302
eman89 <emmanuelsunil89@gmail.com>
2016-10-12 12:18:11 +0200

    use desvs also when asas and lnav are active so that you can specify desired vs with the ALT command

--- bluesky/traf/traffic.py
+++ bluesky/traf/traffic.py
@@ -925,14 +925,11 @@ class Traffic:
                   np.maximum(3.,np.abs(2. * simdt * np.abs(self.vs))) # 3.[m] = 10 [ft] eps alt   
                   
         # TO DO: ADD some vertical speed dynamics so that the desired VS can't be obtained instantly
-        # if asas is not active AND VNAV is not active, then it should use the standard climb rate.
-        # In this case, desvs is not considered - hence a flag is required to prevent
-        # that aircraft taking off start climbing before they have reached v_2
         # if asas is active AND VNAV is not active it should use asasvs (which is desvs)
         # if asas is not active and VNAV is active desvs
         # if asas AND VNAV is active then use desvs
         self.vs = swaltsel*np.sign(self.desalt-self.alt)* \
-                  ((1-self.asas.asasactive)*(1-self.swvnav)*np.abs(1500./60*ft)*self.limvs_flag+\
+                  ((1-self.asas.asasactive)*(1-self.swvnav)*np.abs(self.desvs)*self.limvs_flag+\
                    self.asas.asasactive*(1-self.swvnav)*np.abs(self.desvs)+\
                    (1-self.asas.asasactive)*self.swvnav*np.abs(self.desvs)+\
                    self.asas.asasactive*self.swvnav*np.abs(self.desvs))
