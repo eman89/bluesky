96f7d357cb220baeefb6bab5066ce092d6da2b94
eman89 <emmanuelsunil89@gmail.com>
2016-10-26 18:46:51 +0200

    fix to CR OFF

--- bluesky/traf/asas/DoNothing.py
+++ bluesky/traf/asas/DoNothing.py
@@ -5,8 +5,23 @@ Created on Wed Mar 04 14:27:44 2015
 @author: Jerom Maas
 """
 
+import numpy as np
+from ...tools.aero import ft, nm
+
 def start(dbconf):
     pass
 
 def resolve(dbconf, traf):
-    pass
+    # If resolution is off, and detection is on, and a conflict is detected
+    # then asas will be active for that airplane. Since resolution is off, it
+    # should then follow the auto pilot instructions.     
+    dbconf.asastrk = traf.atrk
+    dbconf.asasspd = traf.aptas
+    dbconf.asasalt = traf.apalt
+    
+    # for vs, first check if it needs to change altitude
+    # if it does, then use the default climb rate    
+    swaltsel = np.abs(traf.apalt-traf.alt) > 0.0
+    dbconf.asasvsp = swaltsel*np.sign(traf.apalt-traf.alt)*(3000.*ft/(10.*nm)*traf.gs)
+    
+    return
