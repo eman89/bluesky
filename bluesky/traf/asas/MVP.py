7ce381347d2262a7a73c7ecbc0118798204251ae
eman89 <emmanuelsunil89@gmail.com>
2017-08-17 13:58:05 +0200

    code cleaning of MVP and vertical resolution fixed
    vertical CR still 2x

--- bluesky/traf/asas/MVP.py
+++ bluesky/traf/asas/MVP.py
@@ -5,7 +5,7 @@ Created on Tue Mar 03 16:50:19 2015
 @author: Jerom Maas
 """
 import numpy as np
-from ...tools.aero import ft
+#from ...tools.aero import ft, kts, fpm
 
 
 def start(dbconf):
@@ -14,15 +14,20 @@ def start(dbconf):
 def resolve(dbconf, traf):
     """ Resolve all current conflicts """
     
+    # Preliminary Calculations-------------------------------------------------
+    
     # Check if ASAS is ON first!    
     if not dbconf.swasas:
         return
-
-    # Initialize an array to store the resolution velocity vector for all A/C
+    
+    # Initialize array to store the resolution velocity vector for all A/C
     dv = np.zeros((traf.ntraf,3)) 
-
-    # If possible, solve conflicts once and copy results for symmetrical conflicts
-    # If that is not possible, solve each conflict twice, once for each A/C
+    
+    
+    # Call MVP function to resolve conflicts-----------------------------------
+    
+    # When ADS-B is off, calculate resolutions for both aircraft in a conflict 
+    # pair in 1 go as resolution will be symetric in this case
     if not traf.adsb.truncated and not traf.adsb.transnoise:
         for conflict in dbconf.conflist_now:
             
@@ -66,7 +71,7 @@ def resolve(dbconf, traf):
                     if ac2 in dbconf.resoofflst: # -> Then id2 does not do any resolutions
                         dv[id2] = 0.0    
                     
-                                                               
+    # If ADS-B is on, calculate resolution for each conflicting aircraft individually                                                           
     else:
         for i in range(dbconf.nconf):
             confpair = dbconf.confpairs[i]
@@ -103,19 +108,23 @@ def resolve(dbconf, traf):
                 if dbconf.swresooff:
                     if ac1 in dbconf.resoofflst: # -> Then id1 does not do any resolutions
                         dv[id1] = 0.0                
-                
+    
+    
+    # Determine new speed and limit resolution direction for all aicraft-------     
 
-    # Now we have the resolution velocity vector for all A/C, cartesian coordinates
+    # Resolution vector for all aircraft, cartesian coordinates
     dv = np.transpose(dv)
 
-    # The old speed vector, cartesian coordinates
+    # The old speed vector for all aircraft, cartesian coordinates
     v = np.array([traf.gseast, traf.gsnorth, traf.vs])
 
-    # The new speed vector, cartesian coordinates
+    # The new speed vector, cartesian coordinates (dv = 0 for conflict free a/c)
     newv = dv+v
     
+    
+    # Limit resolution direction if required------------------------------------
+    
     # Compute new speed vector in polar coordinates based on desired resolution 
-    # direction: horizontal or vertical or horizontal+vertical
     if dbconf.swresohoriz: # horizontal resolutions
         if dbconf.swresospd and not dbconf.swresohdg: # SPD only
             newtrack = traf.trk
@@ -138,24 +147,25 @@ def resolve(dbconf, traf):
         newgs    = np.sqrt(newv[0,:]**2 + newv[1,:]**2)
         newvs    = newv[2,:]
         
-    # Cap the velocity
+    # Cap the velocities
     newgscapped = np.maximum(dbconf.vmin,np.minimum(dbconf.vmax,newgs))
+    vscapped    = np.maximum(dbconf.vsmin,np.minimum(dbconf.vsmax,newvs))
     
-    # Cap the vertical speed
-    vscapped = np.maximum(dbconf.vsmin,np.minimum(dbconf.vsmax,newvs))
     
-    # Now assign resolutions to variables in the ASAS class
+    # Determine ASAS module commands for all aircraft--------------------------
+    
+    # Set ASAS module updates
     dbconf.trk = newtrack
     dbconf.spd = newgscapped
     dbconf.vs  = vscapped
     
-    # To update asasalt, tinconf is used. tinconf is a really big value if there is 
+    # To update asasalt, tsolveV is used. tsolveV is a really big value if there is 
     # no conflict. If there is a conflict, tinconf will be between 0 and the lookahead
     # time. Therefore, asasalt should only be updated for those aircraft that have a 
     # tinconf that is between 0 and the lookahead time (i.e., for the ones that are 
     # in conflict). This is what the following code does:
     altCondition = dbconf.tinconf.min(axis=1) < dbconf.dtlookahead
-    asasalttemp  = dbconf.vs*dbconf.tinconf.min(axis=1) + traf.alt
+    asasalttemp = dbconf.vs*dbconf.tsolveV.min(axis=1) + traf.alt
     dbconf.alt[altCondition] = asasalttemp[altCondition]
     
     # If resolutions are limited in the horizontal direction, then asasalt should
@@ -172,11 +182,14 @@ def resolve(dbconf, traf):
     # LAYERS -> CONDITION 2 IS REQUIRED BELOW!
     
            
-#=================================== Modified Voltage Potential ===============
+#======================= Modified Voltage Potential ===========================
            
 
 def MVP(traf, dbconf, id1, id2):
     """Modified Voltage Potential (MVP) resolution method"""
+    
+    
+    # Preliminary calculations-------------------------------------------------
     
     # Get distance and qdr between id1 and id2
     dist = dbconf.dist[id1,id2]
@@ -187,32 +200,26 @@ def MVP(traf, dbconf, id1, id2):
    
     # Relative position vector between id1 and id2
     drel = np.array([np.sin(qdr)*dist, \
-                np.cos(qdr)*dist, \
-                traf.alt[id2]-traf.alt[id1]])
+                     np.cos(qdr)*dist, \
+                     traf.alt[id2]-traf.alt[id1]])
        
     # Write velocities as vectors and find relative velocity vector              
     v1 = np.array([traf.gseast[id1], traf.gsnorth[id1], traf.vs[id1]])
     v2 = np.array([traf.gseast[id2], traf.gsnorth[id2], traf.vs[id2]])
     vrel = np.array(v2-v1) 
     
-    # Find tcpa (or should it be tinconf, since tinconf decided whether its a conflict?)
-    tcpa = dbconf.tcpa[id1,id2] # dbconf.tinconf[id1,id2]
     
-    # Find horizontal and vertical distances at the tcpa
+    # Horizontal resolution----------------------------------------------------
+    
+    # Get the time to solve conflict horizontally -> tcpa
+    tcpa = dbconf.tcpa[id1,id2] 
+    
+    # Find horizontal distance at the tcpa (min horizontal distance)
     dcpa  = drel + vrel*tcpa
     dabsH = np.sqrt(dcpa[0]*dcpa[0]+dcpa[1]*dcpa[1])
-    dabsV = abs(dcpa[2])
     	
-    # Compute horizontal and vertical intrusions
-#    iH = dbconf.Rm / np.abs(np.cos(np.arcsin(dbconf.Rm / dist) - np.arcsin(dabsH / dist))) - dabsH
+    # Compute horizontal intrusion
     iH = dbconf.Rm - dabsH
-    iV = dbconf.dhm - dabsV
-        
-    # If id1 and id2 are in intrusion, assume full intrusion to force max movement
-    if drel[0] < dbconf.Rm or drel[1] < dbconf.Rm:
-        iH = dbconf.Rm
-    if drel[2] < dbconf.dhm:
-        iV = dbconf.dhm
     
     # Exception handlers for head-on conflicts
     # This is done to prevent division by zero in the next step
@@ -220,39 +227,52 @@ def MVP(traf, dbconf, id1, id2):
         dabsH = 10.
         dcpa[0] = 10.
         dcpa[1] = 10.
-    if dabsV <= 10.:
-        dabsV = 10. 
-        if dbconf.swresovert: # only trigger vertical resolution if it is the desired resolution direction
-            dcpa[2] = 10.
-
-    # Compute the resolution velocity vector in all three directions
-    dv1 = (iH*dcpa[0])/(abs(tcpa)*dabsH)  # abs(tcpa) since tinconf can be positive, while tcpa can be be negative (i.e.,conflcit is behind the two aircraft). A negative tcpa would direct dv in the wrong direction.
+        
+    # Compute the resolution velocity vector in horizontal direction
+    # abs(tcpa) because it bcomes negative during intrusion
+    dv1 = (iH*dcpa[0])/(abs(tcpa)*dabsH)  
     dv2 = (iH*dcpa[1])/(abs(tcpa)*dabsH)
-    dv3 = (iV*dcpa[2])/(abs(tcpa)*dabsV)    
+    
+    # If intruder is outside the ownship PZ, then apply extra factor
+    # to make sure that resolution does not graze IPZ
+    if dbconf.Rm<dist and dabsH<dist:
+        erratum=np.cos(np.arcsin(dbconf.Rm/dist)-np.arcsin(dabsH/dist))
+        dv1 = dv1/erratum
+        dv2 = dv2/erratum
+        
+    
+    # Vertical resolution------------------------------------------------------
+    
+    # Compute the  vertical intrusion
+    # Amount of vertical intrusion dependent on vertical relative velocity
+    iV = dbconf.dhm if abs(vrel[2])>0.0 else dbconf.dhm-drel[2]
+    
+    # Get the time to solve the conflict vertically - tsolveV
+    tsolV = dbconf.tsolveV[id1,id2]
+    
+    # Compute the resolution velocity vector in the vertical direction
+    # The direction of the vertical resolution is such that the aircraft with
+    # higher climb/decent rate reduces their climb/decent rate    
+    dv3 = np.where(abs(vrel[2])>0,  (iV/tsolV)*(-vrel[2]/abs(vrel[2])), (iV/tsolV))
     
     # It is necessary to cap dv3 to prevent that a vertical conflict 
     # is solved in 1 timestep, leading to a vertical separation that is too 
     # high (high vs assumed in traf). If vertical dynamics are included to 
     # aircraft  model in traffic.py, the below three lines should be deleted.
-    mindv3 = -400./60.*ft # ~ 2.016 [m/s]
-    maxdv3 = 400./60.*ft
-    dv3 = np.maximum(mindv3,np.minimum(maxdv3,dv3))
+#    mindv3 = -400*fpm# ~ 2.016 [m/s]
+#    maxdv3 = 400*fpm
+#    dv3 = np.maximum(mindv3,np.minimum(maxdv3,dv3))
+
+    
+    # Combine resolutions------------------------------------------------------
 
     # combine the dv components 
     dv = np.array([dv1,dv2,dv3])
     
-     # Intruder outside ownship IPZ
-    if dbconf.Rm<dist and dabsH<dist:
-        erratum=np.cos(np.arcsin(dbconf.Rm/dist)-np.arcsin(dabsH/dist))
-        dv_plus1 = dv[0]/erratum
-        dv_plus2 = dv[1]/erratum
-	   # combine dv_plus components. Note: erratum only applies to horizontal dv components
-        dv_plus = np.array([dv_plus1,dv_plus2,dv[2]])		
-    # Intruder inside ownship IPZ
-    else: 
-        dv_plus=dv
-          
-    return dv_plus    
+    import pdb
+    pdb.set_trace()
+    
+    return dv
     
 #============================= Priority Rules =================================    
     
